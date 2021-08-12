e869d9c17f78b52fd20a75796195e38e9ec6531e
Vishnu S Reddy <vishnusreddy007@gmail.com>
2021-08-12 13:04:04 +0530

    Updates

---
+++ precache-manifest.fb0c77fd391366d9104ace3d1e7665ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74324f51ca9e3f60fcc32f6d420e7567",
+    "url": "/mkd/index.html"
+  },
+  {
+    "revision": "5d7f3f5522fb10477008",
+    "url": "/mkd/static/css/main.b8ec917d.chunk.css"
+  },
+  {
+    "revision": "772eb65684977b58bec3",
+    "url": "/mkd/static/js/2.651c90a5.chunk.js"
+  },
+  {
+    "revision": "fd66ba2d54ebcf1e0b593646e2dd8e51",
+    "url": "/mkd/static/js/2.651c90a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d7f3f5522fb10477008",
+    "url": "/mkd/static/js/main.93e60b84.chunk.js"
+  },
+  {
+    "revision": "27a1706c53911e6af72e",
+    "url": "/mkd/static/js/runtime-main.03582c70.js"
+  },
+  {
+    "revision": "c654049a4e7fc081b6e73a3c9f3ff111",
+    "url": "/mkd/static/media/github.c654049a.svg"
+  },
+  {
+    "revision": "21b19a9aae2af4d7ea9549dfd943fa36",
+    "url": "/mkd/static/media/theme_icon.21b19a9a.svg"
+  },
+  {
+    "revision": "165de949a05faa29df506ab447351315",
+    "url": "/mkd/static/media/tick.165de949.svg"
+  }
+]);
